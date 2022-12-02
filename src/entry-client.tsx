fb34d8984a349ba1a659492b7d2bd5834ddd679b
isZachariah <103608503+isZachariah@users.noreply.github.com>
2022-12-02 07:18:22 -0800

    added dist

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
-import {Home} from "./pages/Home";
-
-// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//     <App Component={Home()}/>
-// )
 import { BrowserRouter } from "react-router-dom";
-
 import { Router } from "./router";
+
 
 ReactDOM.hydrateRoot(
     document.getElementById("root") as HTMLElement,
@@ -17,3 +11,7 @@ ReactDOM.hydrateRoot(
         <Router />
     </BrowserRouter>
 );
+
+// ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+//     <App Component={Home()}/>
+// )
